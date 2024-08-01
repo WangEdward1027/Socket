7e98b0125c3f6fe4cbc30bea87458d4c0593b142
WangEdward1027 <948096711@qq.com>
2024-08-01 22:52:03 +0800

    业务逻辑被封装到EventLoop::loop()中的逻辑中,无法在test()函数中修改

--- Reactor/ReactorV2/TestEventLoop.cpp
+++ Reactor/ReactorV2/TestEventLoop.cpp
@@ -9,23 +9,6 @@
 using std::cout;
 using std::endl;
 
-void test0()
-{
-    Acceptor acceptor("127.0.0.1", 8888);
-    acceptor.ready(); //处于监听状态
-
-    //三次握手已建立,可以创建一条TCP连接
-    TcpConnection con(acceptor.accept());
-
-    //加调试,打印本端与对端的ip与端口号
-    cout << con.toString() << "has connected" << endl;
-
-    while(1){
-        cout << ">>recv msg from client: " << con.receive() << endl;
-        con.send("hello Edward\n");
-    }
-}
-
 void test(){
     Acceptor acceptor("127.0.0.1", 8888);
     acceptor.ready();  //此时处于监听状态
